c8dbe0d301f9da6b55ad80ddb236513bece54dfd
aiyougg <145369965+aiyougg@users.noreply.github.com>
2026-04-21 21:52:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -30,8 +29,10 @@ umask 022
 git checkout
 popd
 
-#bandix
+#bandix|argon
 cd package
+git clone https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config.git
 git clone https://github.com/timsaya/luci-app-bandix.git
 git clone https://github.com/timsaya/openwrt-bandix.git
 cd ..
