0ebf9c5919f8b804e4a2f18ac41da286cfc7944f
aiyougg <145369965+aiyougg@users.noreply.github.com>
2026-08-27 18:53:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,9 +25,11 @@
 #popd
 
 #bandix|argon
+#oaf
 cd package
 #git clone https://github.com/jerrykuku/luci-theme-argon.git
 #git clone https://github.com/jerrykuku/luci-app-argon-config.git
+git clone https://github.com/destan19/OpenAppFilter.git
 git clone https://github.com/timsaya/luci-app-bandix.git
 git clone https://github.com/timsaya/openwrt-bandix.git
 git clone https://github.com/hudra0/qosmate.git
